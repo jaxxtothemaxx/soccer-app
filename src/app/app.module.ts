77d5b6425d0bdabac7a7fee6c73a456dc32ce905
JaxxtotheMaxx <offbeat5024@gmail.com>
2020-04-07 21:20:22 -0400

    Configure routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PlayersComponent } from './players/players.component';
+
+const appRoutes: Routes = [
+  { path: 'players', component: PlayersComponent },
+];
 
 @NgModule({
   declarations: [
@@ -11,8 +16,12 @@ import { PlayersComponent } from './players/players.component';
     PlayersComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
